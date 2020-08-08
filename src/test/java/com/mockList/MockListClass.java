c9878c6c1b0678128333006517b9e096eedb5971
hariomyadav <hoc.yadav@gmail.com>
2020-08-08 18:12:16 +0530

    nice mock concept

--- src/test/java/com/mockList/MockListClass.java
+++ src/test/java/com/mockList/MockListClass.java
@@ -9,9 +9,9 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class MockListClass {
-	
+
 	@Test
-	public void test1() {
+	public void mockListClass_andSize() {
 		//mock List class
 		List listObj = mock(List.class);
 		//when-then
@@ -19,5 +19,26 @@ public class MockListClass {
 		//test
 		System.out.println(listObj.size());//2
 		assertEquals(2, listObj.size());
+	}
+
+	@Test
+	public void mockListClass_andGet() {
+		//mock List class
+		List list = mock(List.class);
+		//when-then
+		when(list.get(0)).thenReturn("hariom");
+		//test
+		System.out.println(list.get(0));//hariom
+		assertEquals("hariom", list.get(0));
+	}
+
+	@Test
+	public void mockListClass_NiceMockConcept() {
+		List list = mock(List.class);
+		System.out.println(list.get(0));//null
+		System.out.println(list.get(1));//null
+
+		assertEquals(null, list.get(0));
+		assertEquals(null, list.get(1));
 	}
 }
