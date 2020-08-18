abf48bb15fa24fed6867b27dfffcebbfb35afe28
Hariom Yadav <hariom.yadav@halodoc.com>
2020-08-18 08:52:43 +0530

    mock list class

--- src/test/java/com/mockList/MockListClass.java
+++ src/test/java/com/mockList/MockListClass.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 //import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.*;
 
